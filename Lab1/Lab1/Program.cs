44e78c899966c98e8e9c41d3e35541807862fc70
Przemysław Dylewski <pdylewski@edu.cdv.pl>
2025-10-11 13:20:32 +0200

    Zadanie 1 - Warunki

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Zadanie 1. Warunki
+const int requiredAgeEntry = 14;
+const int requiredAgeBuySim = 18;
+const string accessDeniedEntryMessage = "You have to be at least 14 years old to enter my shop";
+const string accessDeniedBuySim = "You have to be at least 18 years old to buy sim card. Welcome";
+const string accessAllowed = "Welcome to my shop";
 
-Console.WriteLine("Hello, World!");
+var age = 0;
+var success = false;
+
+while (!success) {
+    Console.WriteLine("Enter your age:");
+    var input = Console.ReadLine();
+    success = int.TryParse(input, out age);
+}
+
+switch (age)
+{
+    case < requiredAgeEntry:
+        Console.WriteLine(accessDeniedEntryMessage);
+        break;
+    case < requiredAgeBuySim:
+        Console.WriteLine(accessDeniedBuySim);
+        break;
+    default:
+        Console.WriteLine(accessAllowed);
+        break;
+}
